ce0a5b1f5f9990d18d0fd985055d51439e86dcdc
Victor Duarte <victorctii217@gmail.com>
2022-05-05 18:45:05 -0300

    Card component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+
 import Card from './components/Card';
+import cardCSS from './components/Card.css';
 
 function App() {
   return (
@@ -8,9 +10,13 @@ function App() {
       <h1>React-Redux exercises</h1>
       
       <div className="line">
-        <Card />
-        <Card />
-        <Card />
+        <Card title='Card 1' red>X</Card>
+        <Card title='Card 2' green>X</Card>
+      </div>
+
+      <div className="line">
+        <Card title='Card 3' blue>X</Card>
+        <Card title='Card 4' purple>X</Card>
       </div>
 
     </div>
